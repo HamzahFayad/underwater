3bb4f2df4b3d87ac7dfde112fe79d0d70fe281b7
Hamzah Fayad <hamzahfayad99@gmail.com>
2023-04-30 19:54:51 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Underwater</h1>
+      <div className='underwater'>
+        <img src="/water.png" alt="Underwater"/>
+      </div>
+      <div className="content"></div>
+      <div className="content2"></div>
+      <div className="content3"></div>
+
+    </div>
+  );
+}
+
+export default App;
